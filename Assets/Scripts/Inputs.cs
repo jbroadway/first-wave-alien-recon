40269d5d4bcd84c0429b65e00230bbdeb5299aa7
jbroadway <johnny@johnnybroadway.com>
2017-01-21 11:15:53 -0600

    Added basic movement

---
+++ Assets/Scripts/Inputs.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inputs : MonoBehaviour {
+
+	public GameObject earth;
+
+	public float movementSpeed = 0.05f;
+	
+	// Update is called once per frame
+	void Update () {
+		float addX = 0f;
+		float addZ = 0f;
+
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			addX = movementSpeed;
+		} else if (Input.GetKey (KeyCode.RightArrow)) {
+			addX = -movementSpeed;
+		}
+
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			addZ = movementSpeed;
+		} else if (Input.GetKey (KeyCode.UpArrow)) {
+			addZ = -movementSpeed;
+		}
+
+		earth.transform.position = new Vector3 (
+			earth.transform.position.x + addX,
+			earth.transform.position.y,
+			earth.transform.position.z + addZ
+		);
+	}
+}
